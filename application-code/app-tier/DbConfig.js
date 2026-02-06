30697e9d22646a168c196872b261fbc1a45f181d
Naveen J N <158978426+Naveezzz@users.noreply.github.com>
2026-02-06 08:07:11 +0530

    2

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  host: 'my-rds-demo.c3sc2y2iq0tr.ap-south-1.rds.amazonaws.com',  // Replace with your RDS endpoint
-  user: 'admin',                                        // Replace with your RDS username
-  password: 'Naveen2003jn',                            // Replace with your RDS password
-  database: 'webappdb',                                 // Database name
+  host: 'Your rds end point',  // Replace with your RDS endpoint
+  user: 'RDS User Name',                                        // Replace with your RDS username
+  password: 'RDS password',                            // Replace with your RDS password
+  database: 'RDS DB Name',                                 // Database name
   port: 3306,                                           // MySQL default port
   waitForConnections: true,
   connectionLimit: 10,
